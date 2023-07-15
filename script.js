b0ae6cf8a66a2475cf9600d7c3eb749d6bcc3c69
RandomDemon <104325602+RandomDemon@users.noreply.github.com>
2023-07-15 13:19:26 -0400

    Update script.js

--- script.js
+++ script.js
@@ -11,16 +11,28 @@ function rollDice() {
   var oddsElement = document.getElementById('odds');
   var odds = parseFloat(oddsElement.value);
 
+  // Check if the user has enough balance to place the bet
+  if (balance < bet) {
+    alert("You don't have enough balance to place this bet.");
+    return;
+  }
+
   // Generate a random number between 0 and 1 to simulate the roll
   var rollResult = Math.random();
+
+  // Calculate the payout multiplier based on the selected odds
+  var payoutMultiplier = 1 + odds;
 
   // Check if the roll is a win or loss based on the odds
   if (rollResult <= odds) {
     // Win case
-    balance += bet;
+    var winnings = bet * payoutMultiplier;
+    balance += winnings;
+    alert('Congratulations! You won ' + winnings.toFixed(2) + ' credits.');
   } else {
     // Loss case
     balance -= bet;
+    alert('Sorry, you lost ' + bet.toFixed(2) + ' credits.');
   }
 
   // Update the balance textbox with the new balance
